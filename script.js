fecf81b054c406c9dd0240e3a2642b4196c59274
manuela-kamphorst <145712354+manuela-kamphorst@users.noreply.github.com>
2024-06-06 16:25:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-principal");
+const caixaAlternativas = document.querySelector(".caixa-principal");
+const caixaResultado = document.querySelector(".caixa-principal");
+const textoResultado = document.querySelector(".caixa-principal");
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+]
